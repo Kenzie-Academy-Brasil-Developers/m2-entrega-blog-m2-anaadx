ef12a83ed65c4cdacc96c8ff21569119f7d8ab6d
leovgs <leovgsouza@gmail.com>
2022-08-25 18:37:54 -0300

    feat: add function doLogin

---
+++ src/scripts/requests.js
@@ -0,0 +1,32 @@
+class ApiRequests {
+
+    static baseUrl = "https://blog-m2.herokuapp.com"
+    static token = localStorage.getItem("S5-10: token")
+    static headers = {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${this.token}`
+    }
+
+    static async login(body) {
+
+        const userLogin = await fetch (`${this.baseUrl}/users/login`,{
+
+           method: "POST", 
+           headers: this.headers,
+           body: JSON.stringify(body)
+        })
+
+        .then(res => res.json())
+        .then(res => {
+            console.log(res)
+            localStorage.setItem("S5-10: userId", res.userId)
+            localStorage.setItem("S5-10: token", res.token)
+            
+        })
+
+        .catch(err => console.log(err))
+        return userLogin
+    }
+}
+
+export {ApiRequests}
