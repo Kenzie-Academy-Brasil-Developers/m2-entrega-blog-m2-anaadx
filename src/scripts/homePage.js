8dfbd76806280cbd9551a23101d2ff5c0cf095fb
anaadx <anapaula.assisduarte.99@gmail.com>
2022-08-27 12:35:07 -0300

    Merge branch 'develop' of github.com:Kenzie-Academy-Brasil-Developers/m2-entrega-blog-m2-anaadx into developTemporaria

---
+++ src/scripts/homePage.js
@@ -0,0 +1,74 @@
+import { ApiRequests } from "./requests.js";
+import { RenderDesktop, RenderMobile} from "./render.js"
+
+class homePage{
+
+    static renderHomeMobile (posts) {
+        const token = localStorage.getItem("S5-10: token")
+        const postsList = document.querySelector(".container__posts1")
+
+        postsList.innerHTML = ''
+
+        if(!token) {
+            window.location.assign("../../index.html")
+        }
+
+        RenderMobile.renderPostsListMobile(posts)
+    }
+
+    static renderHomeDesktop (posts) {
+        const token = localStorage.getItem("S5-10: token")
+        const postsList = document.querySelector(".container__posts2")
+
+        postsList.innerHTML = ''
+
+        if(!token) {
+            window.location.assign("../../index.html")
+        }
+
+        RenderDesktop.renderPostsListDesktop(posts)
+    }
+
+    static renderCreatePost (){
+        const text = document.getElementById("inputCaixaDeTexto")
+        const btnPost = document.getElementById("btnPost")
+
+        btnPost.addEventListener("click", () => {
+            
+
+            const newPost = {
+                content: text.value
+            }
+            
+        ApiRequests.createPost(newPost)
+        })
+    }
+
+    static logout (){
+        const btnLogout = document.getElementById("btnLogout")
+        btnLogout.addEventListener("click", (event) =>{
+            event.preventDefault()
+            localStorage.clear()
+            window.location.assign("../../index.html")
+        })
+    }
+
+    static async userInfo (){
+        const userImg = document.getElementById("userImg")
+        const userName = document.getElementById("userName")
+        const user = await ApiRequests.getUser()
+
+        userImg.src = user.avatarUrl
+        userName.innerText = user.username
+    }
+}
+
+const posts = await ApiRequests.getPosts("page=1")
+
+homePage.renderHomeDesktop(posts.data)
+homePage.renderHomeMobile(posts.data)
+homePage.renderCreatePost()
+
+homePage.userInfo()
+homePage.logout()
+
